98e845f987e7ac1012cb8e17599aebc15707bd40
MrTylerjet <MrTylerjet@gmail.com>
2021-08-24 18:40:15 -0700

    double ensureing the tmp folder is created cause cp says it doesn't exist

--- install-eapcontroller/install-eapcontroller.sh
+++ install-eapcontroller/install-eapcontroller.sh
@@ -191,6 +191,10 @@ echo " done."
 echo "Patch omada-start.jar"
 mkdir /tmp/omada-start-jar
 sleep 3
+if [ ! -f /tmp/omada-start-jar]; then 
+mkdir /tmp/omada-start-jar
+fi
+sleep 2
 if [ ! -f /opt/tplink/EAPController/lib/omada-start.jar.bak ]; then
     cp "/opt/tplink/EAPController/lib/omada-start.jar" "/opt/tplink/EAPController/lib/omada-start.jar.bak"
 fi
