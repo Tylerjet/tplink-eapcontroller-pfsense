4e4728d0ad270231c741da7523d1ffb152840fef
MrTylerjet <MrTylerjet@gmail.com>
2021-08-24 18:10:27 -0700

    trying to fix cp command being one whole line and saying the path doesn't exists when it does

--- install-eapcontroller/install-eapcontroller.sh
+++ install-eapcontroller/install-eapcontroller.sh
@@ -192,13 +192,13 @@ echo -n "Patch omada-start.jar"
 OMADA_START_JAR="/opt/tplink/EAPController/lib/omada-start.jar"
 mkdir /tmp/omada-start-jar
 if [ ! -f /opt/tplink/EAPController/lib/${OMADA_START_JAR}.bak ]; then
-    cp "/opt/tplink/EAPController/lib/${OMADA_START_JAR}" "/opt/tplink/EAPController/lib/${OMADA_START_JAR}.bak"
+    cp "/opt/tplink/EAPController/lib/${OMADA_START_JAR}" " /opt/tplink/EAPController/lib/${OMADA_START_JAR}.bak"
 fi
-cp "/opt/tplink/EAPController/lib/${OMADA_START_JAR}" "/tmp/omada-start-jar/"
+cp "/opt/tplink/EAPController/lib/${OMADA_START_JAR}" " /tmp/omada-start-jar/"
 ( cd /tmp/omada-start-jar/ && jar -xf ${OMADA_START_JAR} )
 /usr/bin/fetch -o /tmp/omada-start-jar/com/tplink/omada/start ${RC_SCRIPT_URL}
 ( cd /tmp/omada-start-jar/ && jar -cvf ${OMADA_START_JAR} * )
-cp "/tmp/omada-start-jar/${OMADA_START_JAR}" "/opt/tplink/EAPController/lib/${OMADA_START_JAR}"
+cp "/tmp/omada-start-jar/${OMADA_START_JAR}" " /opt/tplink/EAPController/lib/${OMADA_START_JAR}"
 echo " done."
 
 # If partition size is < 4GB, add smallfiles option to mongodb
